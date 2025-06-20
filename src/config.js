4a1b6438e4aabb86d29fa6cb85e93c4dfef5f102
Nasir Khalid <nasir.khalid.new@gmail.com>
2025-06-21 04:07:52 +0500

    changed emails and logos

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'nasir.khalid.new@@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/nasirkhalidq',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/nasirkhalid007',
     },
     {
       name: 'Twitter',
@@ -16,7 +16,7 @@ module.exports = {
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/nasirkhalidq',
     },
     {
       name: 'Codepen',
