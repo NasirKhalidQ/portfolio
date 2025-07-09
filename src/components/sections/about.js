0659853568acdce8fa5b7eed26344856c4588f8d
Nasir Khalid <nasir.khalid.new@gmail.com>
2025-07-10 04:52:13 +0500

    updated about me

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,16 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'NextJS',
+    'React',
+    'JavaScript (ES6+)',
+    'TypeScript',
+    'MeteorJS',
+    'Node.js',
+    'MongoDB',
+    'Angular',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,29 +145,29 @@ const About = () => {
           <div>
             <p>
               Hello! My name is Nasir and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              interest in web development started back in 2020 when I took a remote internship at
+              Code For Asia for 3 months — this is where I got introduced to React and I absolutely
+              loved it's simplicity!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://www.inspon.com/">a SaaS solutions provider</a>,{' '}
+              <a href="https://sg.linkedin.com/company/code-for-asia-society-ltd">a start-up</a>,{' '}
+              <a href="https://brij.it/">
+                a dashboard for Brands to create user experiences for products
+              </a>
+              , and <a href="https://unmudl.com/">a skills-to-job marketplace</a>. My main focus
+              these days is building localized version of instagram{' '}
+              <a href="https://wanoafrica.com/">Wano</a> for the African subcontinent using React
+              Native Expo.
             </p>
 
             <p>
               I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              <a href="https://apps.shopify.com/textfield">redesigned a shopify plugin</a> that
+              allows users to configure different html elements using NextJS &amp; react-hook-form
+              to place them on their Shopify store.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -65,23 +65,19 @@ const Hero = () => {
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a software engineer specializing in building and architecting exceptional digital
+        experiences. Currently, I’m focused on building a content sharing platform for African
+        subcontinent at{' '}
+        <a href="https://wanoafrica.com/" target="_blank" rel="noreferrer">
+          Wano Africa
         </a>
         .
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="https://wanoafrica.com/" target="_blank" rel="noreferrer">
+      Check out our mobile app!
     </a>
   );
 
